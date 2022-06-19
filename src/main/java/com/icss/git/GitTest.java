d8bf481354a5e4eac2fd27f51a36af4ee4137305
sz <supersz@163.com>
2022-06-19 18:54:09 +0800

    third commit

--- src/main/java/com/icss/git/GitTest.java
+++ src/main/java/com/icss/git/GitTest.java
@@ -12,5 +12,7 @@ package com.icss.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
+        System.out.println("hello git2!!!");
+        System.out.println("hello git3!!!");
     }
 }
