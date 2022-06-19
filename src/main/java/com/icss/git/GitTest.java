d14df8bfe4a14feb1603dc110a6529aa6a105adb
sz <supersz@163.com>
2022-06-19 18:29:33 +0800

    first commit

---
+++ src/main/java/com/icss/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.icss.git;
+
+/**
+ * Copyright (C) @2022 Dreamworks Technology
+ *
+ * @author: sz
+ * @version: 1.0
+ * @date: 2022/6/19
+ * @time: 18:20
+ * @description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
